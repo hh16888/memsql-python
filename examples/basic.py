672c9a01f31f330b0c31af11032bf7f17efd569c
H <hocklim@gmial.com>
2023-12-11 14:36:40 +0000

    ss

--- examples/basic.py
+++ examples/basic.py
@@ -1,4 +1,4 @@
 from memsql.common import database
 
-conn = database.connect(host="127.0.0.1", port=3306, user="root", password="")
+conn = database.connect(host="127.0.0.1", port=3306, user="root", password="SingleStore!23")
 print(conn.query("show databases"))
